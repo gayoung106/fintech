e8bc3245dbc8e510287599b5968d1171030e3a39
SongGayoung <xjongam1x@gmail.com>
2023-09-15 14:57:22 +0900

    로그인 암호화 기능

---
+++ fintech_server/expressServer.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mysql = require("mysql2");
+var jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
+const app = express();
+
+dotenv.config();
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_ACCOUNT,
+  password: process.env.DB_PASSWORD,
+  database: "fintech",
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("hello");
+});
+
+app.post("/login", (req, res) => {
+  const { userAccount, password } = req.body;
+  const sql =
+    "SELECT user_id, user_account, user_password FROM fintech.user WHERE user_account = ?";
+  connection.query(sql, [userAccount], (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    let encPassword = sha256Enc(password, "fintech");
+    if (encPassword === result[0].user_password) {
+      let tokenKey = "f@i#n%tne#ckfhlafkd0102test!@#%";
+      jwt.sign(
+        {
+          userId: result[0].user_id,
+          userEmail: result[0].user_account,
+        },
+        tokenKey,
+        {
+          expiresIn: "10d",
+          issuer: "fintech.admin",
+          subject: "user.login.info",
+        },
+        function (err, token) {
+          if (err) {
+            console.error(err);
+          }
+          console.log("로그인 성공", token);
+          res.json(token);
+        }
+      );
+    } else {
+      res.json("비밀번호 다릅니다.");
+    }
+  });
+});
+
+const sha256Enc = (plainText, key) => {
+  const secret = key;
+  const hash = crypto
+    .createHmac("sha256", secret)
+    .update(plainText)
+    .digest("base64");
+  return hash;
+};
+
+app.listen("4001");
